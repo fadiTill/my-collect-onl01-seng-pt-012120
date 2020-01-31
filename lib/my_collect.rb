824e046bdd0b0307464563f1b366f1fd0c6e4870
fadimata tillman <fadi.tillman@gmail.com>
2020-01-31 16:45:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,10 @@
 my_collect(empty_array)
- i = 0
- while i < empty_array.lenght 
-   yield (empty_array|i|)
-   i +=1
- end
- end
+# i = 0
+ #while i < empty_array.lenght 
+   #yield (empty_array|i|)
+   #i +=1
+ #end
+ #end
+ my_collect(empty_array) do |x|
+         counter += 1
+       end
