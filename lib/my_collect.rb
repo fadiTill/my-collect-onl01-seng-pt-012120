7e559cd812317dfd07505df377998edb965bbafc
fadimata tillman <fadi.tillman@gmail.com>
2020-01-31 16:47:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 my_collect(empty_array)
 i = 0
   collection = []
-  while i < array.length
-    collection << yield(array[i])
+  while i < empty_array.length
+    collection << yield(empty_array[i])
     i += 1
   end
 end
